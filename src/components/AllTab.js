fdae27bd06fc44b31919723718d9a85fbc4c5fb9
Wheatley <tiarra.stout@gmail.com>
2018-05-17 18:28:46 -0600

    fixed tabs formatting, need to fix active class transitions still

--- src/components/AllTab.js
+++ src/components/AllTab.js
@@ -8,21 +8,13 @@ export default class AllTabs extends Component {
         return (
             <div>
                 <Tabs selected={0}>
-                    <Pane label='All'>
-                        <div>All</div>       
-                    </Pane>
-                    <Pane label='Simulations'>
-                        <div>Simulations</div>
-                    </Pane>
-                    <Pane label='JS Assessments'>
-                        <div>JS Assessments</div>
-                    </Pane>
-                    <Pane label='Projects'>
-                        <div>Projects</div>
-                    </Pane>
-                    <Pane label='Job Prep'>
-                        <div>Job Prep</div>
-                    </Pane>
+                    <Pane label='All' content='Lorem ipsum dolor sit amet, sententiae appellantur mei ad, falli efficiendi no cum. Duo comprehensam conclusionemque ex, copiosae perpetua dissentias ad ius. Id putant oportere iracundia eam, in pri tamquam copiosae. Ut adhuc tacimates his. Eos erat adhuc at, ne qui velit aeterno ceteros, his simul latine scribentur eu. Ne ius suas graeco reprehendunt, pri et nusquam senserit.
+
+Sonet viris ea vel. Illud civibus ex eos. Odio case altera ut eum. Vis omnesque quaestio te.'/>      
+                    <Pane label='Simulations' content='Simulations content'/>
+                    <Pane label='JS Assessments' content='JS Assessment stuff'/>
+                    <Pane label='Projects' content='Projects and schtuff'/>
+                    <Pane label='Job Prep' content='all the jobs'/>
                 </Tabs>
             </div>
         )
--- src/components/Tabs/Pane.js
+++ src/components/Tabs/Pane.js
@@ -2,9 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Pane(props){
+    console.log(props.content, 'content pane')
     return(
-        <div>
-            {props.children}
+        <div className='pane'>
+        this thing doesn't do anything at all. Why did they even have me make it? It like it's just a place holder. 
+            {props.content}
         </div>
     )
 }
